2b8dc3aba66bbd7574a089b84998e004d21cfd1a
Benjamin Schweizer <bschweiz@gmail.com>
2020-10-12 22:13:17 -0500

    tried getting the multiplication to work on the harvested objects, having issues and will look at it with fresh eyes in the AM

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,7 +1,11 @@
 
 export const harvestPlants = (plantsArray) => {
+    const newPlantArray = []
     for (const x of plantsArray) {
-        console.log(x.output)
+        for (let i = 0; i = x.output; i++) {
+            newPlantArray.push(x)
+            
+        }
     } 
-    
+    console.log(newPlantArray)
 }
