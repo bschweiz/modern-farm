6a2ccdbbc25fd83bc39038cc2228f4d3a4504506
Benjamin Schweizer <bschweiz@gmail.com>
2020-10-14 14:15:21 -0500

    figured out the proper way to import and call harvester.js in main.js so that now i pass all the npm tests. Huzzah.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 console.log("Welcome to the main module")
 
 import { createPlan } from "./plan.js"
-import { addPlant, usePlants } from "./field.js";
+import { usePlants } from "./field.js";
 import { plantSeeds } from "./tractor.js"
 import { harvestPlants } from "./harvester.js"
 
@@ -32,4 +32,6 @@ console.log(yearlyPlan);
 plantSeeds(yearlyPlan);
 
 
-harvestPlants(test);
+const harvestToSell = harvestPlants(test);
+
+console.log(harvestToSell);
